2442dd0ed8b1f4c231476f75317ef30f4daaa747
Diana_Bobrova <dziana_babrova@epam.com>
2025-04-22 00:10:30 +0300

    feat: complete fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'fs/promises';
+import { Buffer } from 'node:buffer';
+import path from 'path';
+import * as url from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathName = path.join(__dirname, '/files/fresh.txt');
+  const data = new Uint8Array(Buffer.from('I am fresh and young'));
+
+  try {
+    const promise = writeFile(pathName, data, { flag: 'wx' });
+    await promise;
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
