f08b7bb0bc4ae9880a6f0e0d3510d79c08417043
Diana_Bobrova <dziana_babrova@epam.com>
2025-04-22 22:54:41 +0300

    feat: complete fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { cp } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const sourcePath = path.resolve(__dirname, 'files');
+  const destinationPath = path.resolve(__dirname, 'files_copy');
+  try {
+    await cp(sourcePath, destinationPath, {
+      force: false,
+      errorOnExist: true,
+      recursive: true,
+    });
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -5,12 +5,11 @@ import * as url from 'url';
 
 const create = async () => {
   const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-  const pathName = path.join(__dirname, '/files/fresh.txt');
+  const pathName = path.resolve(__dirname, 'files/fresh.txt');
   const data = new Uint8Array(Buffer.from('I am fresh and young'));
 
   try {
-    const promise = writeFile(pathName, data, { flag: 'wx' });
-    await promise;
+    await writeFile(pathName, data, { flag: 'wx' });
   } catch {
     throw new Error('FS operation failed');
   }
