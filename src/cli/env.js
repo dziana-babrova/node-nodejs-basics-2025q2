f9c870293e2e0e7d0e1faae14ac4bc0d83b6ceac
Diana_Bobrova <dziana_babrova@epam.com>
2025-04-24 00:30:07 +0300

    feat: complete cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const result = Object.entries(env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(result);
 };
 
 parseEnv();
