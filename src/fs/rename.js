b7936f6431152e2f803f3dece94c244b9bed2751
Diana_Bobrova <dziana_babrova@epam.com>
2025-04-23 23:26:51 +0300

    feat: complete fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import { rename as renameFile, constants, access } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const fileExists = async (filePath) => {
+  try {
+    await access(filePath, constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const oldPath = path.resolve(__dirname, 'files/wrongFilename.txt');
+  const newPath = path.resolve(__dirname, 'files/properFilename.md');
+
+  try {
+    if (!(await fileExists(oldPath))) throw new Error();
+    if (await fileExists(newPath)) throw new Error();
+    await renameFile(oldPath, newPath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
