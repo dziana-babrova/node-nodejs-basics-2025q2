1ee042f7c89e8d3779d0b47b158a2f3b6fcd86fe
Diana_Bobrova <dziana_babrova@epam.com>
2025-04-24 23:49:12 +0300

    feat: complete streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+import * as url from 'url';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathName = path.resolve(__dirname, 'files/fileToWrite.txt');
+  const stream = createWriteStream(pathName, 'utf-8');
+  stdin.pipe(stream);
 };
 
 await write();
