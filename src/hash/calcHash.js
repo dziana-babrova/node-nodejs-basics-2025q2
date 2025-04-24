3db5fe0f4203ff2274033e76d425157c4edbf8b7
Diana_Bobrova <dziana_babrova@epam.com>
2025-04-24 23:21:55 +0300

    feat: complete hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import * as url from 'url';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathName = path.resolve(__dirname, 'files/fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const stream = createReadStream(pathName);
+
+  stream.on('readable', () => {
+    const data = stream.read();
+    if (data) hash.update(data);
+    else {
+      console.log(hash.digest('hex'));
+    }
+  });
 };
 
 await calculateHash();
